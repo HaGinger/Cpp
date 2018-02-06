a70da84af1d1ee9210fea85e535453af0e2d3d5a
HaGinger <35551187+HaGinger@users.noreply.github.com>
2018-02-06 11:17:38 +0800

    Update coordinate.h

--- coordinate.h
+++ coordinate.h
@@ -5,8 +5,8 @@ class coord//同一半球且不跨本初子午线
 	double a;//赤道半径
 	double b;//极半径
 	double r;//平均半径
-    double lng;//经度
-    double lat;//纬度
+	double lng;//经度
+	double lat;//纬度
     
 public:
 	coord(double x,double y);
