7d201999e82825e8e93c793372c5a7a683d2e335
HaGinger <35551187+HaGinger@users.noreply.github.com>
2018-02-06 11:16:14 +0800

    Update and rename coordinate to coordinate.h

---
+++ coordinate.h
@@ -0,0 +1,16 @@
+
+
+class coord//同一半球且不跨本初子午线
+{
+	double a;//赤道半径
+	double b;//极半径
+	double r;//平均半径
+    double lng;//经度
+    double lat;//纬度
+    
+public:
+	coord(double x,double y);
+	~coord();
+    double distance(coord des);
+	int neibor(set<coord>s,double rr);
+};
