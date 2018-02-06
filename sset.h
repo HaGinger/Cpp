e6cb79e0e02b4c568c561331f2ae170d968adb7b
HaGinger <35551187+HaGinger@users.noreply.github.com>
2018-02-06 13:20:08 +0800

    Update sset.h

--- sset.h
+++ sset.h
@@ -2,23 +2,18 @@
 
 class sset
 {
-	int * m_ptr;//数组头指针
+	sset * m_ptr;//数组头指针
 	int m_size;//数组实际数据元素个数
 	int m_capacity;//数组当前容量
 	
 	void clear();//清空（或使用初始化数组中的所有元素）
-	void moveItems(int index);//将index之后的所有元素，向后移动一个位置
 	void enlarge();//扩充数组容量
 	
 public:
 	sset(int capacity = 0);//构造函数，默认初始元素为//intSet();
-	sset(const sset & set);//赋值（拷贝）构造函数
 	~sset(void);
+	void insert(sset item);
+	sset operator[](int n);
 	int size();
 };
 
-sset::~sset(void)
-{
-	if(m_ptr != NULL)
-		delete [] m_ptr;
-}
